e984d1b63bb195a173895debdd686c24c7be0e1b
Erika Mesh <esmvcs@rit.edu>
2025-09-11 15:55:30 -0400

    Thurs 9/11 - Screen to world points & mouse position

--- 2D Project/Assets/Scripts/Week3Demo.cs
+++ 2D Project/Assets/Scripts/Week3Demo.cs
@@ -5,7 +5,13 @@ public class Week3Demo : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject ballPrefab;
+    private GameObject goPrefab;
+
+    [SerializeField]
+    Vector3 mousePosition;
+
+    [SerializeField]
+    Vector3 spawnPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,18 +22,26 @@ public class Week3Demo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+        mousePosition = Mouse.current.position.ReadValue();
+        spawnPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+        spawnPoint.z = 0;
+        GameObject newGO = Instantiate(goPrefab, spawnPoint, Quaternion.identity);
+        newGO.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0, 1, 0, 1);
+        */
     }
 
-    public void OnInteract(InputAction.CallbackContext context)
+    public void OnSpawn(InputAction.CallbackContext context)
     {
         Debug.Log("Hi!! - " + context.phase);
         if (context.phase == InputActionPhase.Canceled)
         {
             Debug.Log("** GO **");
-            Vector2 newLoc = transform.position;
-            GameObject ball = Instantiate(ballPrefab, newLoc, Quaternion.identity);
-            ball.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0, 1, 0, 1);
+            mousePosition = Mouse.current.position.ReadValue();
+            spawnPoint = Camera.main.ScreenToWorldPoint(mousePosition);
+            spawnPoint.z = 0;
+            GameObject newGO = Instantiate(goPrefab, spawnPoint, Quaternion.identity);
+            newGO.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1, 0, 1, 0, 1);
         }
     }
 
