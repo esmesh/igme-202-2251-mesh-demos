5523fc331930c1e11e1204f51a23edf895d9f2af
Erika Mesh <esmvcs@rit.edu>
2025-09-25 16:01:40 -0400

    Thurs 9/25 - Random target creation

--- 2D Project/Assets/Scripts/RandomTargets.cs
+++ 2D Project/Assets/Scripts/RandomTargets.cs
@@ -8,10 +8,27 @@ public class RandomTargets : MonoBehaviour
     [SerializeField]
     TextMesh scoreLabel;
 
+    [SerializeField]
+    GameObject tarPrefab;
+
+    [SerializeField]
+    private GameObject target;
+
+    [SerializeField]
+    Vector3 targetCenter = Vector3.zero;
+
+    [SerializeField]
+    Vector3 targetBoundsSize = Vector3.zero;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Vector3 location = new Vector3(
+            Random.Range(targetCenter.x - targetBoundsSize.x / 2, targetCenter.x + targetBoundsSize.x / 2),
+            Random.Range(targetCenter.y - targetBoundsSize.y / 2, targetCenter.y + targetBoundsSize.y / 2),
+            0
+            );
+        target = Instantiate(tarPrefab, location, Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -23,5 +40,15 @@ public class RandomTargets : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         score++;
+        Destroy(target);
+        target = null;
     }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(targetCenter, targetBoundsSize);
+    }
+
 }
